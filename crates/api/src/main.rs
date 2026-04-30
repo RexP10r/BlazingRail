d546064aacb716094e4d84dc153d0d5abdada455
rexp10r <rexp10r@localhost.localdomain>
2026-04-30 18:23:32 +0300

    feat(api): tracing launch debug

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -2,9 +2,10 @@ use anyhow::Result;
 use axum::{Router, routing::post};
 use common::{AppConfig, EventInput};
 use dotenvy::dotenv;
-use tracing_subscriber::EnvFilter;
+use std::env;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::{net::TcpListener, sync::mpsc::channel};
+use tracing_subscriber::EnvFilter;
 
 mod error;
 
@@ -21,6 +22,11 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
+
+    tracing::info!(
+        "Telemetry initialized. Log level: {}",
+        env::var("RUST_LOG").unwrap_or_else(|_| "default".to_string())
+    );
 
     let config = Arc::new(AppConfig::new());
 
