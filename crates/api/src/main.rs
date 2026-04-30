5adc86e632db5ee5849299ef6f8e8203a595338f
rexp10r <rexp10r@localhost.localdomain>
2026-04-30 17:42:19 +0300

    feat(api main): logic updated with new structures and enums

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use axum::{Router, routing::post};
+use common::{AppConfig, EventInput};
 use std::{net::SocketAddr, sync::Arc};
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, sync::mpsc::channel};
 
 mod error;
 
@@ -13,13 +14,16 @@ use handler::handle_create_event;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let state = Arc::new(AppState::new());
+    let config = Arc::new(AppConfig::new());
 
+    let (tx, _rx) = channel::<EventInput>(config.channel_capacity);
+
+    let state = Arc::new(AppState::new(config.clone(), tx));
     let app: Router = Router::new()
         .route("/v1/events", post(handle_create_event))
         .with_state(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.server_port));
     println!("Server launched on {}", &addr);
 
     let listener = TcpListener::bind(addr).await?;
